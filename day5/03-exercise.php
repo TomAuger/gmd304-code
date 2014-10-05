74103d76c9733349faa21d6c3c47071403ae1d9d
Tom Auger <tom.auger@zeitguys.com>
2014-10-05 19:19:47 -0400

    Exercise 3

---
+++ day5/03-exercise.php
@@ -0,0 +1,53 @@
+Encrypted with Vignere passphrase cipher KOD
+http://rumkin.com/tools/cipher/vigenere.php
+
+<?qhg
+$walmv = brijy( "Swp", "Kov", "Mam", "Jrc", "Jzv", "Thu", "Vpmv xtgmu hup fisp d serulx trog ejmd!" );
+
+// 1.
+nrserlh( $mipfs rb $nzuh ){
+$seu = aaml(0, 255);
+$jsevw = rzvg(0, 255);
+$clln = rzvg(0, 255);
+
+fcyx "<soiq ttpue='bwopr:ipb($qmg,$hrvnn,$atxf)'>$nrve </rxdo>";
+}
+?>
+
+<hi>
+
+
+<?yho
+// 2.
+nrs ( $i = txumb( $qbmvb ) - 1; $i > -1; $h = $q - 3 ){
+hdhf "<y>$i: {$mipfs[$z]}</y>";
+}
+?>
+
+<hq>
+
+<?xkq
+ffaezkk( $oadns za &$qbmv){
+// celxrsairlx zhqlrle zto uhv "x"s vqwi a jhmawo
+$oadn = ssz_lseguabm( "r", "@", $oadn );
+// rdxobcv jlk "q"v xikq "o"r
+$vdne = jcr_hzhqlrle( "h", "w", $qbmv );
+// wov zhqlrle zto uhv byljrms nrtg "q"v
+$oadn = ssz_lseguabm( "@", "l", $oadn );
+
+// Im kdte nn rdxobcvm tgm ijrjc ldbwfr
+$ejmd = cfgiibt( $mipf );
+}
+pirns_z( $qbmvb );
+?>
+
+<hq>
+
+<?xkq
+$nrver = iusap( "Col", "Rrf", "Drw", "Bnj", "Mjm", "Krm", "Amq", "Oib" );
+
+ncgw "<rm>";
+ffaezkk( $oadns za $qbmv ){
+ncgw "<oj>" . utoiqaw( tticokwzfr( jcr_rpxgfcn( $nzuh) ) ) . " | " . vcwrrrb( vurkxlnehs( skardd( $qbmv) ) ) . "</ui>";
+}
+dkkp "</oc>";
