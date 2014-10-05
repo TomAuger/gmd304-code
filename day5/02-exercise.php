e4e97ee328617727dc1871b6797defe697181e69
Tom Auger <tom.auger@zeitguys.com>
2014-10-05 14:52:24 -0400

    Exercise 1 (ciphered - sorry ya cheaties!)

---
+++ day5/02-exercise.php
@@ -0,0 +1,40 @@
+Encrypted with Vignere passphrase cipher
+http://rumkin.com/tools/cipher/vigenere.php
+
+<?oez
+$peabarvuhw = bqoks(
+"dmvmp" => klhyz( "zmzfu", "ffkfmifrfq", "ybuyl", "toxqburuh", "ykld", "cdgl", "pchc uqrme", "qquqlxukr", "gklgyh" ),
+"yeibmnytfr" => xblqw( "qqbdno", "bjrdemjgof", "isnjjf", "prswa", "bjqqi", "zqbfc", "psgfjf" ),
+"ubbvi" => ysqxi( "mdggebn", "ujc", "ktdqfub", "uhzufub", "qtkmbub", "tgrxhub", "jfkylub", "jlfdujce" )
+);
+
+$mleh_1 = $lmdzyefqpz['mlehi'][pbma( 0, miklu( $ulmursmzoi['hesor'] ) - 1 )];
+$xnduauhso_1 = $peabarvuhw['bcgowjgwdp'][budb( 0, dnrxn( $lmdzyefqpz['zatysrjubc'] ) - 1 )];
+
+$pupc = $ulmursmzoi['pupcr'][okht( 0, aptkd($peabarvuhw['wdolm'] ) - 1 )];
+
+$dmvm_2 = $sywqzvkxbs['dmvmp'][budb( 0, dnrxn( $lmdzyefqpz['mlehi'] ) - 1 )];
+$yeibmnytf_2 = $ulmursmzoi['uthfbqspuq'][szkn( 0, wesos( $sywqzvkxbs['qbkdzdclct'] ) - 1 )];
+
+dzri "<f>";
+cdgl "Dbu $yeibmnytf_1 $mleh_1 $lcsa qry $qbkdzdclc_2 $onrx_2.";
+ysfp "</o>";
+
+
+// =================================================
+// Oozqaunoshw 1 - Rbjb cicc pe qry bycnrb ikr pe fd
+
+zkldsfyh wcu_qxxxek_xnon( $jqpu_nc_cjucdg ){
+dvirym $ulmursmzoi;
+lurvqk $fisyctiklo[$nbqq_yz_infdzr][lqle( 0, blehj( $tpbxlobysx[$mklj_mg_rmoysf] ) - 1 )];
+}
+
+$onrx_1 = aur_szknic_upqa( 'xiklt' );
+$zatysrjub_1 = qyj_pbmayg_mmsc( 'xnduauhsom' );
+$lcsa = don_hyoclw_qepe( 'ubbvi' );
+$lptk_2 = qyj_pbmayg_mmsc( 'kyodq' );
+$bcgowjgwd_2 = don_hyoclw_qepe( 'zatysrjubc' );
+
+ysfp "<o>";
+bmbe "Rid $xnduauhso_1 $heso_1 $ubbv jff $zatysrjub_2 $xikl_2.";
+fbey "</j>";
